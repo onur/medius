924a8aa1184964fd1ab245c5181e69ce8c3e4fe6
Onur Aslan <onuraslan@gmail.com>
2015-09-16 15:10:41 +0300

    Add push-output.sh

---
+++ push-output.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# I don't want to keep revision history for this folder
+# I will just remove gh-pages and return to first commit and re-add everything
+
+git push origin :gh-pages
+git reset --soft HEAD~1
+git add .
+git push origin gh-pages
